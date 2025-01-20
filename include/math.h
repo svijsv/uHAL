bfa761b6d8bc19bea53a363719595c9a56b8f52d
svijsv <local>
2025-01-20 17:09:49 -0500

    Squashed 'test/lib/ulib/' changes from ebb8f10..8e8fb7d
    8e8fb7d Fix (more) time-handling bugs
    6d4f396 Make GET_LEAPS() and IS_LEAP_YEAR() static as they're supposed to be
    fe7454c Rework utility macros for reading and writing registers and volatiles
    d65e03b Add the signed itime_t type and UTIME_MAX and ITIME_MAX macros
    501988a Add FIXED_POINT_FROM_INT_FRACTION() and fixed_point_from_int_fraction()
    6fcb04d Add fraction and whole-number bit masks for fixed-point integers
    f5702f3 Fix PRINTF_BUFFER_BYTES calculation
    1b8c6a2 Add support for the Microsoft printf extensions %I8, %I16, %I32, and %I64
    409f7e1 Add FIXED_POINT_REPLACE_WITH_FLOAT
    cc5cb49 Move fixed-point stuff into it's own module
    4669861 Rename fixed_pnt and FIXED_PNT to fixed_point and FIXED_POINT
    eb5c663 Panic on halloc() heap overruns
    e607ea9 Fix definition of ulib_panic()
    5cb9d2b Fix build warning when DEBUG not set
    a6cb397 Make it easier to override configuration settings
    f79063d Use the ASCII header for cstring_from_ulib()
    
    git-subtree-dir: test/lib/ulib
    git-subtree-split: 8e8fb7da2531c353d42aeffd6fca2350d0dc128c

--- include/math.h
+++ include/math.h
@@ -2,7 +2,7 @@
 /***********************************************************************
 *                                                                      *
 *                                                                      *
-* Copyright 2022, 2023 svijsv                                          *
+* Copyright 2022, 2023, 2025 svijsv                                    *
 * This program is free software: you can redistribute it and/or modify *
 * it under the terms of the GNU General Public License as published by *
 * the Free Software Foundation, version 3.                             *
@@ -29,7 +29,6 @@
 #if ULIB_ENABLE_MATH
 
 #include "types.h"
-#include "util.h"
 
 
 /*
@@ -100,119 +99,6 @@ uint64_t div_u64_u16(uint64_t n, uint16_t d);
 uint64_t div_u64_u32(uint64_t n, uint32_t d);
 uint64_t div_u64_u64(uint64_t n, uint64_t d);
 int64_t  div_s64_s64( int64_t n,  int64_t d);
-
-/*
-* Fixed-point math operations
-*/
-// fixed_pnt_t is the type used to store representations.
-// fixed_pnt_math_t is the type used for operations that may overflow fixed_pnt_t.
-#if FIXED_PNT_BITS > 64
-# error "FIXED_PNT_BITS is too big"
-#elif FIXED_PNT_BITS == 64
-# define FIXED_PNT_MIN INT64_MIN
-# define FIXED_PNT_MAX INT64_MAX
-  typedef int64_t fixed_pnt_t;
-  // Maybe some day I'll be able to count on int128_t...
-  //typedef __int128 fixed_pnt_math_t;
-  typedef int64_t fixed_pnt_math_t;
-#elif FIXED_PNT_BITS == 32
-# define FIXED_PNT_MIN INT32_MIN
-# define FIXED_PNT_MAX INT32_MAX
-  typedef int32_t fixed_pnt_t;
-  typedef int64_t fixed_pnt_math_t;
-#elif FIXED_PNT_BITS == 16
-# define FIXED_PNT_MIN INT16_MIN
-# define FIXED_PNT_MAX INT16_MAX
-  typedef int16_t fixed_pnt_t;
-  typedef int32_t fixed_pnt_math_t;
-#elif FIXED_PNT_BITS == 8
-# define FIXED_PNT_MIN INT8_MIN
-# define FIXED_PNT_MAX INT8_MAX
-  typedef int8_t fixed_pnt_t;
-  typedef int16_t fixed_pnt_math_t;
-#else
-# error "Unsupported FIXED_PNT_BITS size"
-#endif
-//
-// '1' converted to our fixed-point notation.
-#define FIXED_PNT_1 ((fixed_pnt_t )1 << FIXED_PNT_FRACT_BITS)
-//
-// Create a fixed-point number from an integer
-#define FIXED_PNT_FROM_INT(x) ((fixed_pnt_t )(x) << FIXED_PNT_FRACT_BITS)
-INLINE fixed_pnt_t fixed_pnt_from_int(fixed_pnt_t x) {
-	return x << FIXED_PNT_FRACT_BITS;
-}
-//
-// Create a fixed-point number from a float
-#define FIXED_PNT_FROM_FLOAT(x) ((x) * (float )FIXED_PNT_1)
-INLINE fixed_pnt_t fixed_pnt_from_float(float x) {
-	return (fixed_pnt_t )(x * (float )FIXED_PNT_1);
-}
-//
-// Create an integer from a fixed-point number.
-#define FIXED_PNT_TO_INT(x) ((fixed_pnt_t )(x) >> FIXED_PNT_FRACT_BITS)
-INLINE fixed_pnt_t fixed_pnt_to_int(fixed_pnt_t x) {
-	return x >> FIXED_PNT_FRACT_BITS;
-}
-//
-// Create an integer from a fixed-point number, rounded.
-INLINE fixed_pnt_t fixed_pnt_to_int_rounded(fixed_pnt_t x) {
-	return (x + (FIXED_PNT_1 / 2)) >> FIXED_PNT_FRACT_BITS;
-}
-//
-// Create a float from a fixed-point number.
-INLINE float fixed_pnt_to_float(fixed_pnt_t x) {
-	return (float )x / (float )FIXED_PNT_1;
-}
-//
-// Multiply two fixed-point numbers.
-// (X*Sx) * (Y*Sy) = (X*Y) * (Sx*Sy)
-// The scaling factor is squared, so one needs to be removed
-// ((X*Y) * (S*S)) / S
-#define FIXED_PNT_MUL(x, y) ((fixed_pnt_t )(((fixed_pnt_math_t )(x) * (fixed_pnt_math_t )(y)) >> FIXED_PNT_FRACT_BITS))
-INLINE fixed_pnt_t fixed_pnt_mul(fixed_pnt_math_t x, fixed_pnt_math_t y) {
-	return (fixed_pnt_t )((x * y) >> FIXED_PNT_FRACT_BITS);
-}
-//
-// Multiply a fixed-point number by an integer.
-// Integers are unscaled so no shift is necessary.
-#define FIXED_PNT_MUL_BY_INT(f, n) ((fixed_pnt_math_t )(f) * (n))
-INLINE fixed_pnt_t fixed_pnt_mul_by_int(fixed_pnt_t x, fixed_pnt_t y) {
-	return (x * y);
-}
-//
-// Divide two fixed-point numbers.
-// https:// en.wikipedia.org/wiki/Fixed-point_arithmetic#Operations
-// (X*Sx) / (Y*Sy) = (X/Y) * (Sy/Sx)
-// The scaling factor cancels out, so it needs to be re-added
-// ((X/Y) * (S/S)) * S
-// Use an overridable macro for division because a certain platform doesn't
-// have native support for 64 bit division and using libc adds a full Kb to
-// the .data section of our binary...
-#if !defined(_FIXED_PNT_DIV_PRIM)
-# define _FIXED_PNT_DIV_PRIM(_n, _d) ((_n) / (_d))
-#endif
-#define FIXED_PNT_DIV(n, d) ((fixed_pnt_t )(_FIXED_PNT_DIV_PRIM(((fixed_pnt_math_t )(n) << FIXED_PNT_FRACT_BITS), (fixed_pnt_t )(d))))
-INLINE fixed_pnt_t fixed_pnt_div(fixed_pnt_math_t n, fixed_pnt_math_t d) {
-	return (fixed_pnt_t )_FIXED_PNT_DIV_PRIM((n << FIXED_PNT_FRACT_BITS), d);
-}
-//
-// Divide a fixed-point number by an integer.
-// Integers are unscaled so no shift is necessary.
-#define FIXED_PNT_DIV_BY_INT(f, n) (_FIXED_PNT_DIV_PRIM((fixed_pnt_t )f, n))
-INLINE fixed_pnt_t fixed_pnt_div_by_int(fixed_pnt_t f, fixed_pnt_t n) {
-	return (fixed_pnt_t )_FIXED_PNT_DIV_PRIM(f, n);
-}
-
-//
-// Calculate the natural logarithm of a fixed-point number.
-fixed_pnt_t log_fixed_pnt(fixed_pnt_t x);
-//
-// Calculate the base 10 logarithm of a fixed-point number.
-fixed_pnt_t log10_fixed_pnt(fixed_pnt_t x);
-//
-// Calculate the base 2 logarithm of a fixed-point number.
-fixed_pnt_t log2_fixed_pnt(fixed_pnt_t x);
 
 
 #endif // ULIB_ENABLE_MATH
